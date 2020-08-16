7a18c5b61dbe116c47dbd84ddf8fb7b399bea1ed
duydinhv <53370450+duydinhv@users.noreply.github.com>
2020-08-16 10:11:59 +0700

    Initial Commit

---
+++ MyMusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusicApp
+//
+//  Created by DUY on 8/16/20.
+//  Copyright © 2020 DUY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
